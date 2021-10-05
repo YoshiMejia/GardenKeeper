c80c4ebc9ec1dc4b7b5506754b90b7071ed305dd
github username <yoshimejia00@gmail.comgit config --global user.name Yoshi>
2021-10-04 21:17:35 -0400

    started sessionscontroller build

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
     has_many :plants, through: :gardens
     has_many :plots
     has_many :plants, through: :plots
+    has_secure_password
+    #remember to add validations before completing
 end
