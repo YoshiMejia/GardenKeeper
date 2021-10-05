9adb2ec74e367b2b8f5de17d2dbe20099922e064
github username <yoshimejia00@gmail.comgit config --global user.name Yoshi>
2021-10-04 20:39:21 -0400

    changed column name

--- db/migrate/20211005000949_create_plots.rb
+++ db/migrate/20211005000949_create_plots.rb
@@ -1,7 +1,7 @@
 class CreatePlots < ActiveRecord::Migration[6.1]
   def change
     create_table :plots do |t|
-      t.string :type
+      t.string :vegetation_type
       t.integer :plant_id
       t.integer :user_id
       t.integer :garden_id
