69789466998dce0eff439d7ad8821b73c4cc38ec
github username <yoshimejia00@gmail.comgit config --global user.name Yoshi>
2021-10-05 13:06:59 -0400

    building plant controller

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -1,2 +1,18 @@
 class PlantsController < ApplicationController 
+    def new
+        @plant = Plant.new
+        #should i add hidden_field to assign user to plant?
+        #maybe add conditional, so if there is a user_id, then assign to the plant via hidden field.
+    end
+
+    def create
+        byebug
+        @plant = Plant.new(plant_params)
+        redirect_to 
+    end
+
+    private
+    def plant_params
+        params.require(:plant).permit(:name, :description)
+    end
 end
