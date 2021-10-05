e97b918081b0658fd44fd10d1f68d93d149c5a61
github username <yoshimejia00@gmail.comgit config --global user.name Yoshi>
2021-10-04 20:39:08 -0400

    fixed association

--- app/models/plant.rb
+++ app/models/plant.rb
@@ -2,5 +2,5 @@ class Plant < ApplicationRecord
     has_many :gardens
     has_many :users, through: :gardens
     has_many :plots
-    has_many :gardens, through: :plots
+    has_many :users, through: :plots
 end
