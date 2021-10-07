c7a968915e0b7bc99cadfe8b262bf248b1185ee9
github username <yoshimejia00@gmail.comgit config --global user.name Yoshi>
2021-10-07 12:09:39 -0400

    added current_user helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,6 +3,10 @@ module ApplicationHelper
         !!session[:user_id]
     end
 
+    def current_user
+        @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+      end
+
     # def no_data?
     #     @user = User.find_by(id: session[:user_id])
     #     !!@user.garden || !!@user.plants || !!@user.plots
