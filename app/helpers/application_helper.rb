39f7b97c33130a20c891e4d6d20be4cb1cd9fa87
github username <yoshimejia00@gmail.comgit config --global user.name Yoshi>
2021-10-08 15:45:53 -0400

    nil

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,9 +7,9 @@ module ApplicationHelper
         @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
     end
 
-    def empty_garden?
-        self.plants.nil?
-    end
+    # def empty_garden?
+    #     self.plants.nil?
+    # end
 
 
     # def no_data?
--- app/models/plant.rb
+++ app/models/plant.rb
@@ -2,4 +2,5 @@ class Plant < ApplicationRecord
     has_many :gardens
     has_many :users, through: :gardens
     validates :name, :description, presence: true
+
 end
