f554034d0f0dbbafd973e8e85992708d95699860
github username <yoshimejia00@gmail.comgit config --global user.name Yoshi>
2021-10-09 18:46:09 -0400

    created error message to check for nested route in view

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,9 +7,9 @@ module ApplicationHelper
         @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
     end
 
-    # def empty_garden?
-    #     self.plants.nil?
-    # end
+    def nested_route?
+        !!params[:user_id]
+    end
 
 
     # def no_data?
