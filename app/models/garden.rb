f282bab7e2315e40b3e113c343e8b112351514c4
github username <yoshimejia00@gmail.comgit config --global user.name Yoshi>
2021-10-09 18:45:53 -0400

    added error message to validtion

--- app/models/garden.rb
+++ app/models/garden.rb
@@ -2,7 +2,7 @@ class Garden < ApplicationRecord
     belongs_to :user
     belongs_to :plant
     validates :address, presence: true
-    validates :plant, uniqueness: {scope: :address}
+    validates :plant, uniqueness: {scope: :address, message: "error: You already have this plant saved at this address!" }
     accepts_nested_attributes_for :plant, reject_if: proc { |attributes| attributes['name'].blank? }
     
 end
