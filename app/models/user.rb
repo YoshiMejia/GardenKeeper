e0a9f35738ed5ca40926f88fab7fbb2e94853b36
github username <yoshimejia00@gmail.comgit config --global user.name Yoshi>
2021-10-09 18:36:27 -0400

    changed wording

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,6 @@ class User < ApplicationRecord
     has_secure_password
     validates :email, uniqueness: true
     validates :name, :email, presence: true
+
     #remember to add validations(require name etc) before completing
 end
