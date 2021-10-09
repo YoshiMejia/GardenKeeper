a0d09b14ee774d1e9d1b1a84a6cd54bdacde943f
github username <yoshimejia00@gmail.comgit config --global user.name Yoshi>
2021-10-09 18:35:59 -0400

    same plant cannot be added to the same address

--- app/models/garden.rb
+++ app/models/garden.rb
@@ -2,6 +2,7 @@ class Garden < ApplicationRecord
     belongs_to :user
     belongs_to :plant
     validates :address, presence: true
+    validates :plant, uniqueness: {scope: :address}
     accepts_nested_attributes_for :plant, reject_if: proc { |attributes| attributes['name'].blank? }
     
 end
