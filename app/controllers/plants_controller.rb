3d35ec98b6fe1acd3478595805b3d8dc8d46747f
github username <yoshimejia00@gmail.comgit config --global user.name Yoshi>
2021-10-10 16:37:25 -0400

    actions not being used

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -1,8 +1,5 @@
 class PlantsController < ApplicationController 
-    # def new
-    #     @plant = Plant.new
-    # end
-
+  
     def index
         @plants = Plant.all
     end
@@ -11,27 +8,8 @@ class PlantsController < ApplicationController
         @plant = Plant.find(params[:id])
     end
 
-    # def create
-    #     @plant = Plant.find_or_create_by(plant_params)
-    #     if @plant.save
-    #         @plant.users << current_user
-    #         redirect_to user_plant_path(current_user, @plant)
-    #     else
-    #         render :new
-    #     end
-    # end
-
     def celebrity
         @celebrity = Plant.celebrity
     end
-#     def destroy
-# #<%= button_to "Delete plant", user_plant_path(current_user, @plant), method: :delete %><br><br>
-#         Plant.find(params[:id]).destroy
-#         redirect_to user_gardens_path(current_user)
-#     end
-    
-    # private
-    # def plant_params
-    #     params.require(:plant).permit(:name, :description)
-    # end
+
 end
