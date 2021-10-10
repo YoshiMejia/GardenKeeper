2d9e9d7df7f343a9e89f749ae14453ff1b60a77b
github username <yoshimejia00@gmail.comgit config --global user.name Yoshi>
2021-10-09 21:57:22 -0400

    another scope method

--- app/models/plant.rb
+++ app/models/plant.rb
@@ -3,4 +3,8 @@ class Plant < ApplicationRecord
     has_many :users, through: :gardens
     validates :name, :description, presence: true
 
+    def self.sort_names
+        order(:name)
+    end
+
 end
