64d567c642137cb97a976c5f2ffd612e383e43a7
github username <yoshimejia00@gmail.comgit config --global user.name Yoshi>
2021-10-10 16:37:34 -0400

    created helper for Garden views

---
+++ app/helpers/gardens_helper.rb
@@ -0,0 +1,33 @@
+module GardensHelper
+    #helper to see other users plants aka Plant.all without current users plants
+    #to be used in the gardens/new collection
+    def other_plants
+        plants = current_user.plants
+           plants.map do |p|
+           @other_plants = Plant.where.not(name: p.name)
+       end
+       @other_plants ||= Plant.all
+    end
+
+    def display_owner(user)
+        if user != current_user 
+           content_tag(:p, "#{user.name} owns this plant.")
+       end 
+   end
+
+   def delete_button(garden)
+        if garden.user == current_user 
+        (button_to "Delete this garden", user_garden_path(current_user, garden), method: :delete)
+        end 
+   end
+
+    def index_header
+        if !!params[:user_id] && current_user.gardens.empty?
+            content_tag(:h1, "Hey, #{current_user.name}!") + content_tag(:h3, "Create a garden first, and then you'll see it here!")
+        elsif !!params[:user_id] 
+            content_tag(:h1, "Hey, #{current_user.name}! Here are your gardens.")
+        else
+            content_tag(:h1, "All Gardens")
+        end
+    end
+end
