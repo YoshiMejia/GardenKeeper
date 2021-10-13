a57c072a8419202019b4540d2ecf8d8e03b24ec6
github username <yoshimejia00@gmail.comgit config --global user.name Yoshi>
2021-10-12 20:56:48 -0400

    added route for about page

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   post '/logout', to: 'sessions#destroy'
-
+  get '/about', to: 'users#about'
   get '/signup', to: 'users#new'
   post '/signup', to: 'users#create'
 
